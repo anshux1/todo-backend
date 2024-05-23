80564cf1b04623ed9e1860705bc74ea3985ffd29
Anshu Kumar Singh <anshukrsingh11@gmail.com>
2024-05-23 19:14:43 +0530

    Created todos schema

---
+++ src/models/todo.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  completed : {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const todo = mongoose.model("todo", todoSchema);
